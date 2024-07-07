0789c9c5b1215231e5fa2d3c8e731c0b4d1db8b0
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2024-07-07 20:14:56 +0530

    first commit

---
+++ Day_2/do_while_loop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class dowhile{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        int i=1;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=n);/*it will do the work first then check the condition
+it means it will work at least one time either the condition is true or not.*/
+    }
+}
