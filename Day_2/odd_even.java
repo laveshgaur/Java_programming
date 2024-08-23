d6db3f74d261c67b8042c9fe297ad9743918b6dc
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2024-08-23 23:47:22 +0530

    Modified

--- Day_2/odd_even.java
+++ Day_2/odd_even.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class evenodd{
+class odd_even{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number : ");
