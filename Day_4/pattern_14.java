78464ff332040dfae64e6f24090abcaa65e23418
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2024-08-24 00:09:40 +0530

    Pattern 14

---
+++ Day_4/pattern_14.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class pattern_14{
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter line number : ");
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=i;j<n;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int j=i;j<=n;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
