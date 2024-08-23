b124ed5811b37f0562487a1844d2e7dd4bb1ff41
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2024-08-24 00:07:30 +0530

    Pattern 11

---
+++ Day_4/pattern_11.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class pattern_11{
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter line number : ");
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for (int j=(n-i);j>=1;j--){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
