8d27120473e67207b6b9aba30c704b6fdd2ff3db
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2024-08-23 23:45:34 +0530

    Modified

--- Day_2/n_natural_numbers.java
+++ Day_2/n_natural_numbers.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class natural{
+class n_natural_numbers{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number : ");
