8bc0457d7608270b8ec62e7e40f71ac8dceb7eff
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2024-08-23 23:50:50 +0530

    Modified

--- Day_3/pattern_5.java
+++ Day_3/pattern_5.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class pattern{
+class pattern_5{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of lines : ");
