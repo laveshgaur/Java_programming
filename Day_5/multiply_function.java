606c3dd243f443f024708939accbc3903a49f3f5
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2024-09-01 15:04:49 +0530

    Function

---
+++ Day_5/multiply_function.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class functon{
+    public static int multiplyFuncton(int a, int b){
+        return a*b;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number : ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number : ");
+        int b = sc.nextInt();
+        int ans = multiplyFuncton(a, b);
+        System.out.println("Multiply is "+ans);
+    }
+}
