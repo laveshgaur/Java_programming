f561fed4bd21ae8a92fa524ae22f89ea799595dd
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2024-09-21 04:37:05 +0000

    New Method of printing array

---
+++ Day_6/arr2.java
@@ -0,0 +1,11 @@
+class arr2{
+    public static void main(String args[]){
+        int a[] = {1,2,3,4,5};
+        int length = a.length;
+        System.out.print("Length of arr is "+length+"\nElements are : ");
+        for(int x:a){
+            System.out.print(x+" ");
+        }
+        System.out.println();
+    }
+}
