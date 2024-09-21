4488c836405f21640cb9146bf9031d95cdda4214
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2024-09-21 14:11:33 +0530

    Delete an element from array

---
+++ Day_6/deleteElement.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class deleteElement{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements : ");
+        int n = sc.nextInt();
+        System.out.print("Enter elements : ");
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("Enter a number to remove : ");
+        int value = sc.nextInt();
+        for(int i=0;i<n;i++){
+            if(arr[i]==value){
+                for(int j=i;j<n-1;j++){
+                    arr[j]=arr[j+1];
+                }
+                arr[n-1]=0;
+                break;
+            }
+            if(n-1==i){
+                System.out.println("Element is not in array ");
+            }
+        }
+        System.out.print("New array : ");
+        for(int x:arr){
+            System.out.print(x+" ");
+        }
+        System.out.println();
+    }
+}
