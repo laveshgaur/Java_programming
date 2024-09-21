682b6e73b41282c6f0782d38a60cc0637ed2cc8a
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2024-09-21 05:19:35 +0000

    Sum of elements of an array

---
+++ Day_6/arraySum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class arraySum{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements : ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.print("Enter elements : ");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int sum=0;
+        for(int x:arr){
+            sum+=x;
+        }
+        System.out.println("Sum of array is : "+sum);
+    }
+}
