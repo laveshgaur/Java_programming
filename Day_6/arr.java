504972da944bea4c1c52e761c55a48b8ce5d62d3
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2024-09-21 04:20:16 +0000

    Basic array

---
+++ Day_6/arr.java
@@ -0,0 +1,11 @@
+class arr{
+    public static void main(String args[]){
+        int a[] = {1,2,3,4,5};
+        int length = a.length;
+        System.out.print("Length of arr is "+length+"\nElements are : ");
+        for(int i=0;i<length;i++){
+            System.out.print(a[i]+" ");
+        }
+        System.out.println();
+    }
+}
