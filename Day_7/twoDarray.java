c83dba489f6bb5c4f30008c44507f8b39ce9f7a6
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2024-09-22 12:06:09 +0530

    2D Array

--- Day_7/twoDarray.java
+++ Day_7/twoDarray.java
@@ -1,12 +1,11 @@
 class twoDarray{
     public static void main(String args[]){
         int arr[][] = {{2,3,5,8,6},{1,4,7,9,10}};
-        for(int i=0;i<5;i++){
+        for(int i=0;i<2;i++){
             for(int j=0;j<5;j++){
                 System.out.print(arr[i][j]+" ");
             }
             System.out.println();
         }
-        System.out.println();
     }
 }
